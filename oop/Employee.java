6a4c0f84f3a8d1ec5429ab4a696bde4f35df80d4
Iana Popovichenko <ianapopovichenko@Ianas-MacBook-Pro.local>
2026-04-17 14:03:16 +0200

    OOP

---
+++ oop/Employee.java
@@ -0,0 +1,54 @@
+package oop;
+
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Employee {
+    protected final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+    private final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
+    private static String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
+    protected static Pattern peoplePat = Pattern.compile(peopleRegex);
+
+    private String lastName;
+    private String firstName;
+    protected LocalDate dob;
+    protected final Matcher peopleMat;
+
+    public Employee(String personText) {
+        peopleMat = peoplePat.matcher(personText);
+        if (peopleMat.find()) {
+            this.lastName = peopleMat.group("lastName");
+            this.firstName = peopleMat.group("firstName");
+            this.dob = LocalDate.from(dtFormatter.parse(peopleMat.group("dob")));
+        }
+    }
+    // Статический фабричный метод для создания объектов
+    // Flinstone5, Fred5, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
+    public static final IEmployee createEmployee(String employeeText) {
+        Matcher peopleMat = Employee.peoplePat.matcher(employeeText);
+        if (peopleMat.find()) {
+            return switch (peopleMat.group("role")) {
+                case "Programmer" -> new Programmer(employeeText);
+                case "Manager" -> new Manager(employeeText);
+                case "Analyst" -> new Analyst(employeeText);
+                default -> () -> 0;
+            };
+        } else{
+            return () ->0;
+        }
+    }
+
+    public abstract int getSalary();
+
+    public double getBonus() {
+        return getSalary() * 1.10;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s: %s", lastName, firstName, moneyFormat.format(getSalary()));
+    }
+}
---
+++ oop/IEmployee.java
@@ -0,0 +1,5 @@
+package oop;
+
+public interface IEmployee {
+    int getSalary();
+}
